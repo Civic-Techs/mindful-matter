8408b09c35cc6818f4fa483b775ebfcfdb462fb2
Tyron <tyronodame5@gmail.com>
2025-05-06 11:27:26 -0400

    added more to the express file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+// Middleware function for logging route requests
+
+const logRoutes = (req, res, next) => {
+  const time = new Date().toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next(); // Passes the request to the next middleware/controller
+};
+// Register the logRoutes middleware globally to log all requests
+app.use(logRoutes);
+
+// Other endpoints and controllers
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Mindful Matter API!');
+});
+
 const port = 8080;
 
 app.listen(port, () => {
