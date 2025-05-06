730247c43255255196560b45546a4e7908bca6fc
issacbiena <issacbiena@gmail.com>
2025-05-06 22:11:21 +0000

    finished moving code, removed extra files. begining login page and very VERY simple styling for oganaization purposes

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-    return <h1>Home Page</h1>;
-  }
+    // return <h1>Home Page</h1>;
+
+    return (
+      <>
+        <h1>MindfulMotion</h1>
+        <a href="/login">Login</a>
+      </>
+      )}
