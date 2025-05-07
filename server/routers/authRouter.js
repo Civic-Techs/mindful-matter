bee598c9611a837fd7ba71d00ce55e5d4a87b64a
Catalina Dinozo <csdinozo@gmail.com>
2025-05-07 12:34:52 -0400

    routers

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const authControllers = require('../controllers/authController');
-const checkAuthentication = require('../middleware/checkAuthentication');
 
 const authRouter = express.Router();
 
@@ -8,3 +7,5 @@ authRouter.post('/register', authControllers.registerUser);
 authRouter.get('/me', authControllers.showMe);
 authRouter.post('/login', authControllers.login);
 authRouter.delete('/logout', authControllers.logout);
+
+module.exports(authRouter);
