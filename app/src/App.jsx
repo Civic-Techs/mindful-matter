f9aad55ca73da0a7053c0e451895746a603bf093
Ajene <ajenec@gmail.com>
2025-05-08 15:15:11 -0400

    typo

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home';
-import LoginPage from './pages/Login';
-import UserProfile from './pages/UserProfile';
-import ChallengesPage from './pages/Challenges';
-import ChallengeInfo from './components/challengeDetails';
-import NavBar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import ChallengesPage from "./pages/Challenges";
+import ChallengeInfo from "./components/ChallengeDetails";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
